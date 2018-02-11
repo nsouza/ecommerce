24ed49837c1dbd16acd7a79283927f4104ad65dc
Nilo Souza <nilo.contatos@gmail.com>
2018-02-11 21:30:39 -0200

    Tela de login

--- index.php
+++ index.php
@@ -50,6 +50,13 @@ $app->get('/admin/login', function() {
 
 });
 
+$app->post('/admin/login', function(){
+	User::login($_POST["login"], $_POST["password"]);
+	header("Location: /admin");
+	exit;
+});
+
+
 $app->get('/admin/logout', function()
 {
 	User::logout();
